267c5e709b148bedb06f1f2bb318f3f97f5d06bb
Seif Kamal <seif@tickx.co.uk>
2020-05-14 13:18:07 +0100

    Rename package

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::io::stdin;
+
 use termion::input::TermRead;
 use termion::event::{Key, Event, MouseEvent};
 
-use ward::{self, grid::{self, Connect}};
+use shketch::{self, Connect};
 
 enum Tool {
     Brush,
@@ -16,18 +17,18 @@ impl Default for Tool {
 }
 
 fn main() {
-    let mut frame = ward::Frame::default();
-    let mut sketch = grid::Segment::new();
-    let mut toolbar = grid::Segment::new();
-    toolbar += grid::Segment::from_str(grid::Point::new(1, 1), "q - Exit");
-    toolbar += grid::Segment::from_str(grid::Point::new(20, 1), "k - Clear");
-    toolbar += grid::Segment::from_str(grid::Point::new(40, 1), "u - Undo");
-    toolbar += grid::Segment::from_str(grid::Point::new(1, 2), "1 - Brush");
-    toolbar += grid::Segment::from_str(grid::Point::new(20, 2), "2 - Ruler");
+    let mut frame = shketch::Frame::default();
+    let mut toolbar = shketch::Segment::new();
+    toolbar += shketch::Segment::from_str(shketch::Point::new(1, 1), "q - Exit");
+    toolbar += shketch::Segment::from_str(shketch::Point::new(20, 1), "k - Clear");
+    toolbar += shketch::Segment::from_str(shketch::Point::new(40, 1), "u - Undo");
+    toolbar += shketch::Segment::from_str(shketch::Point::new(1, 2), "1 - Brush");
+    toolbar += shketch::Segment::from_str(shketch::Point::new(20, 2), "2 - Ruler");
     frame.layer(&toolbar);
 
-    let tracer = grid::Tracer::default();
-    let mut cursor = grid::Point::default();
+    let tracer = shketch::Tracer::default();
+    let mut sketch = shketch::Segment::new();
+    let mut cursor = shketch::Point::default();
     let mut tool = Tool::default();
 
     for c in stdin().events() {
@@ -40,7 +41,7 @@ fn main() {
             Event::Mouse(mouse_event) => {
                 match mouse_event {
                     MouseEvent::Press(_, a, b) => {
-                        cursor = grid::Point::new(a, b);
+                        cursor = shketch::Point::new(a, b);
                     }
                     MouseEvent::Hold(a, b) => {
                         // Reserve toolbar space
@@ -50,12 +51,12 @@ fn main() {
 
                         match tool {
                             Tool::Brush => {
-                                sketch += tracer.connect(&cursor, &grid::Point::new(a, b));
-                                cursor = grid::Point::new(a, b);
+                                sketch += tracer.connect(&cursor, &shketch::Point::new(a, b));
+                                cursor = shketch::Point::new(a, b);
                             }
                             Tool::Ruler => {
                                 frame.erase(sketch);
-                                sketch = tracer.connect(&cursor, &grid::Point::new(a, b));
+                                sketch = tracer.connect(&cursor, &shketch::Point::new(a, b));
                             }
                         }
                     }
